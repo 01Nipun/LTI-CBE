08441f949ccdeb21ddc24bb01e0750e844896b18
Pavan-gs <pavangs.info@gmail.com>
2026-02-07 10:28:08 +0530

    Add files via upload

---
+++ mybasics.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """Mock function for addition operation."""
+    print(a + b)
+
+
+def multiply(a, b):
+    """Mock function for multiplication operation."""
+    print(a * b)
+
+add(2, 3)
+multiply(4, 5)
